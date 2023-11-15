f2130e625ac6284ed8262faaed6c6acb108a08be
smmr89 <139288948+smmr89@users.noreply.github.com>
2023-11-15 12:40:23 +0000

    completed challenge

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # leaflet-challenge
+
+
+This repository contains solutions to the Leaflet.js challenge Module 15. A plot of earthquakes geeJSON data is performed with an accompanying legend.
+
+## Instructions
+
+* The `index.html`, `static/js/logic.js` and `static/css/style.css` files required for producing the map successfully
+* Open the `index.html` file on a browser or go to https://smmr89.github.io/leaflet-challenge/
+* Circle markers on the Map are sized according to magntiude
+* Circle markers colours on the Map are coloured according to depth (see Legend at bottom right)
+
+## References
+
+Insightful discussions with OpenAI ChatGPT - https://chat.openai.com/
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,32 +1,26 @@
-// all day: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
-// all week: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+// Select which data to use: all day / all week / all month
 
-// let URL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
-let URL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+// let URL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'; // all day
+let URL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'; // all week
+// let URL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'; // all month
 
+// Load in geoJSON data
 d3.json(URL).then(function (response) {
-    console.log(response)
+    // console.log(response)
+    // Call function within the d3 wrapper
     createFeatures(response.features)
 })
 
+// Create Features function to initiate Popups and Markers
 function createFeatures(earthquakeData) {
+    // Popup with Location, Depth and Magnitude
     function onEachFeature(feature, layer) {
         layer.bindPopup(`<h3>${feature.properties.title}</h3>\
             <hr>Magnitude: ${feature.properties.mag}<hr>\
             Depth: ${feature.geometry.coordinates[2]}`)
     }
 
-    // let geojsonMarkerOptions;
-
-    // var geojsonMarkerOptions = {
-    //     radius: 5,
-    //     radius: feature.properties.mag,
-    //     fillColor: "#ff7800",
-    //     color: "#000",
-    //     weight: 1,
-    //     opacity: 1,
-    //     fillOpacity: 0.8
-    // };
+    // Get min and max Depth values for plotting circles
 
     let minDepth = 0;
     let maxDepth = 0;
@@ -42,15 +36,21 @@ function createFeatures(earthquakeData) {
 
 
     let earthquakes = L.geoJSON(earthquakeData, {
+        // Plot the markers using Popup function onEachFeature above
         onEachFeature: onEachFeature,
+        // Use pointToLayer to return circles
         pointToLayer: function (feature, latlng) {
             // console.log(feature.properties.mag)
-            magScale = (feature.properties.mag) * 3;
+            // Size of circles determined by magntiude
+            magScale = (feature.properties.mag) * 2;
+            // Color of circles determined by depth
             depthScale = feature.geometry.coordinates[2];
 
             // return L.circleMarker(latlng, geojsonMarkerOptions);
+            // Call the getColor function to get a color based on the depthScale
             let markerColor = getColor(depthScale);
             // console.log(markerColor)
+            // Input arguments for the circleMarker
             return L.circleMarker(latlng, {
                                         radius: magScale,
                                         color: markerColor,
@@ -60,13 +60,15 @@ function createFeatures(earthquakeData) {
 
         }
     });
-
+    // call createMap function
     createMap(earthquakes);
 }
 
+
+// Define color ranges based on depth
 function getColor(depth) {            
     // https://www.rapidtables.com/web/color/RGB_Color.html
-    let markerColor = '#8B0000'//DARK RED
+    let markerColor = '#8B0000'//DARK RED - default color: if depth 100 or greater
     if (depth < 0) {
         markerColor = '#008000';//GREEN
     } else if (depth < 20) {
@@ -80,10 +82,13 @@ function getColor(depth) {
     } else if (depth < 100) {
         markerColor = '#FF0000';// RED
     }
+
     return markerColor;
 }
 
+// Define createMap function
 function createMap(earthquakes) {
+    // tileLayer, baseMap and overlayMaps
     let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     })
@@ -103,13 +108,51 @@ function createMap(earthquakes) {
     let center_globe = [
         30, 31
     ]
+    // Map is centred on Globe. Use center_USA coordinates if want it centred on USA (and change zoom to level 5)
     let myMap = L.map("map", {
         center: center_globe,
         zoom: 3,
         layers: [street, earthquakes]
     });
-
+    // set control layer for Maps
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
+
+    // Define legend
+    let legend = L.control({ position: 'bottomright' });
+    // Colors:
+    let legend_info = {
+        'Less than 0': '#008000',//GREEN
+        '0-19' : '#00FF00',//LIME
+        '20-39' : '#FFFF00',//YELLOW
+        '40-59' : '#FFA500',//ORANGE
+        '60-79' : '#FF4500',//ORANGE RED
+        '80-99' : '#FF0000',// RED
+        '100 +' : '#8B0000'//DARK RED
+    };
+    
+    // console.log(legend_info)
+    // console.log(Object.keys(legend_info)[0])
+    
+    // Add legend    
+    legend.onAdd = function() {
+        let div = L.DomUtil.create("div", "legend");
+        
+        div.innerHTML = [
+            "<h3>Earthquake Depth (m)</h3><hr>",
+            '<i style="background:' + Object.values(legend_info)[0] + '"></i><b>' + Object.keys(legend_info)[0] + '</b><br>',
+            '<i style="background:' + Object.values(legend_info)[1] + '"></i><b>' + Object.keys(legend_info)[1] + '</b><br>',
+            '<i style="background:' + Object.values(legend_info)[2] + '"></i><b>' + Object.keys(legend_info)[2] + '</b><br>',
+            '<i style="background:' + Object.values(legend_info)[3] + '"></i><b>' + Object.keys(legend_info)[3] + '</b><br>',
+            '<i style="background:' + Object.values(legend_info)[4] + '"></i><b>' + Object.keys(legend_info)[4] + '</b><br>',
+            '<i style="background:' + Object.values(legend_info)[5] + '"></i><b>' + Object.keys(legend_info)[5] + '</b><br>',
+            '<i style="background:' + Object.values(legend_info)[6] + '"></i><b>' + Object.keys(legend_info)[6] + '</b>'
+          ].join(""); 
+        
+        return div;
+      };
+      
+
+    legend.addTo(myMap);
 }
